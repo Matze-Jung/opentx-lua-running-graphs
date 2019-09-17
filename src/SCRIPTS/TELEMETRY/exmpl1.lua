12e3f30bffebb8ba1b466e42aea0b9af401fa759
Matze Jung <52033553+Matze-Jung@users.noreply.github.com>
2019-09-17 23:43:40 +0200

    Update exmpl1.lua, simplify layout

--- src/SCRIPTS/TELEMETRY/exmpl1.lua
+++ src/SCRIPTS/TELEMETRY/exmpl1.lua
@@ -3,69 +3,55 @@ local function run(event)
 
     lcd.clear()
 
-    -- throttle range
-    lcd.drawText(3, 5, "THR", SMLSIZE)
-    lcd.drawText(lcd.getLastPos()+1, 5, "RNG", SMLSIZE)
-    lcd.drawNumber(lcd.getLastPos()+2, 5, getValue("gvar9") > 0 and getValue("gvar9") or 100)
-    lcd.drawText(lcd.getLastPos(), 5, "%", SMLSIZE)
-
-    -- armed
-    if getValue("ls4") > 0 then
-        lcd.drawText(60, 4, "ARMED", BLINK)
-    end
-
-    -- timer one
-    lcd.drawTimer(LCD_W-35, 2, getValue("timer1"), MIDSIZE)
-
     -- tx battery voltage
-    local format = SMLSIZE
+    local format = 0
     if getValue("tx-voltage") <= 0 then
         format = format+BLINK
     end
-    lcd.drawText(3, 17, "BAT  ", format)
-    lcd.drawNumber(28, 17, getValue("tx-voltage")*100, PREC2+format)
-    lcd.drawText(lcd.getLastPos(), 17, "V", format)
+    lcd.drawText(2, 1, "TX BAT", format)
+    lcd.drawNumber(lcd.getLastPos() + 6, 1, getValue("tx-voltage")*100, PREC2+format)
+    lcd.drawText(lcd.getLastPos() + 1, 1, "V", format)
     local settings = getGeneralSettings()
     local percent =  math.floor(((getValue("tx-voltage")-settings.battMin) * 100 / (settings.battMax-settings.battMin)) + 0.5)
     if percent <= 0 then percent = 0 end
 
-    lcd.drawGauge(3, 25, LCD_W-8 , 3, percent, 100)
+    lcd.drawGauge(2, 10, LCD_W-4 , 13, percent, 100)
 
     if percent < 10 then
         percent = "  "..percent
     elseif percent < 100 then
         percent = " "..percent
     end
-    lcd.drawText(LCD_W-23, 17, percent.."%", format)
+    lcd.drawText(LCD_W-20, 1, percent.."%", format)
 
     -- RSSI sensor
-    format = SMLSIZE
+    format = 0
     if rssi <= alarm_crit then
         format = format+BLINK
     end
-    lcd.drawText(3, 31, "RSSI  ", format)
-    lcd.drawNumber(lcd.getLastPos(), 31, rssi, format)
-    lcd.drawText(lcd.getLastPos(), 31, "dB", format)
+    lcd.drawText(2, 27, "RSSI", format)
+    lcd.drawNumber(lcd.getLastPos() + 7, 27, rssi, format)
+    lcd.drawText(lcd.getLastPos(), 27, "dB", format)
 
     local avrg = math.floor(getGraphAverage(1) + 0.5)
     if avrg < 10 then avrg = " "..avrg end
-    lcd.drawText(LCD_W-50, 31, "AVRG  ", SMLSIZE)
-    lcd.drawText(lcd.getLastPos(), 31, avrg, SMLSIZE)
-    lcd.drawText(lcd.getLastPos(), 31, "dB", SMLSIZE)
+    lcd.drawText(LCD_W-54, 27, "AVRG", 0)
+    lcd.drawText(lcd.getLastPos() + 6, 27, avrg, 0)
+    lcd.drawText(lcd.getLastPos(), 27, "dB", 0)
 
 
     createGraph(1, {
         src=function() local rssi = getRSSI() return rssi end,
         x=13,
-        y=40,
-        w=LCD_W-18-4,
-        h=21,
-        speed=100,
+        y=36,
+        w=LCD_W-16,
+        h=27,
+        speed=200,
         min=alarm_crit, max=100,
         crit=alarm_low,
     })
-    lcd.drawText(3, 40, "99", SMLSIZE)
-    lcd.drawText(3, 56, alarm_crit, SMLSIZE)
+    lcd.drawText(2, 36, "99", SMLSIZE)
+    lcd.drawText(2, 56, alarm_crit, SMLSIZE)
 end
 
 return { run=run }
