8b8f844fbf4a9883a04a45bc20ca90828c94ce91
Matze Jung <52033553+Matze-Jung@users.noreply.github.com>
2019-09-25 00:49:11 +0200

    Update build files

---
+++ bin/arc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -d .git ]; then
+    ORIGIN="`git config --get remote.origin.url`"
+    if [[ $ORIGIN == '' ]]; then
+        ORIGIN="`pwd`"
+    fi
+
+    ARCHIVE="releases/$(basename ${ORIGIN//.git/})_$(git describe --abbrev=0 --tag).zip"
+
+    echo "creating archive ..."
+    mkdir -p releases
+    rm -fv $ARCHIVE
+    cd obj
+
+    if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
+        # linux zip
+        zip -r9q "../${ARCHIVE}" ./*
+    else
+        # win 7zip
+        7z a -r -mx9 -bb0 "../${ARCHIVE}" ./*
+    fi
+    cd ..
+    echo
+    echo -e "\e[1m${ARCHIVE}\e[1m\e[0m \e[0m"
+    echo
+fi
