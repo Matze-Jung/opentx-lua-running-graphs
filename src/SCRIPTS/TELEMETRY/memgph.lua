3955e77264c222e9c0fcfec3020f0c37f91f720d
Matze Jung <52033553+Matze-Jung@users.noreply.github.com>
2019-11-08 23:16:15 +0100

    Add TELEMETRY/memgph.lua

---
+++ src/SCRIPTS/TELEMETRY/memgph.lua
@@ -0,0 +1,27 @@
+-- NOTE:  Define global var MEMORY, typically anywhere near the end of the code cycle:
+--        MEMORY = collectgarbage("count") * 1024
+
+local function run(event)
+    lcd.clear()
+
+    lcd.drawFilledRectangle(0, 0, LCD_W, 8)
+    lcd.drawText(1, 1, "MEM USED  "..(MEMORY or 0).."B", SMLSIZE+INVERS)
+    createGraph(6, {
+        src=function() return MEMORY or 0 end,
+        x=17,
+        y=10,
+        w=LCD_W-18,
+        h=LCD_H-19,
+        speed=100,
+        min=25600,
+        max=76800,
+        crit=61440,
+    })
+    lcd.drawText(1, 10, "75K", SMLSIZE)
+    lcd.drawText(1, LCD_H-16, "25K", SMLSIZE)
+
+    lcd.drawFilledRectangle(0, LCD_H-8, LCD_W, 8)
+    lcd.drawText(1, LCD_H-7, "AVRG  "..math.floor(getGraphAverage(6) + 0.5).."B      MAX  "..(getGraphRange(6).max or 0).."B", SMLSIZE+INVERS)
+end
+
+return { run = run }
